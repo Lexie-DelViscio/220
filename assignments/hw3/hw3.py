a0d40e72298aac60da9983d1d16ee7736bbccefc
Lexie-DelViscio <delvisciolp@g.cofc.edu>
2022-02-04 13:40:26 -0500

    hw 3 second commit

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,13 +1,13 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Lexie DelViscio
+hw3.py.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: This program utilizes for loops in order to calculate and return
+a variety of values including grade average, tip amount, square root, and
+an approximation of pi.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
@@ -18,8 +18,8 @@ def average():
     for i in range(number_of_grades):
         user_values = eval(input("enter value: "))
         sum_accumulation = sum_accumulation + user_values
-    average = sum_accumulation / number_of_grades
-    print("The average of your grades is: ", average)
+    average_grades = sum_accumulation / number_of_grades
+    print("The average of your grades is: ", average_grades)
 
 
 def tip_jar():
@@ -31,23 +31,29 @@ def tip_jar():
 
 
 def newton():
-    x = eval(input("What number do you want to square root? "))
-    approximation = x
+    to_square_root = eval(input("What number do you want to square root? "))
+    approximation = to_square_root
     improvement_amount = eval(input("How many times should we improve the approximation? "))
     for i in range(improvement_amount):
-        approximation = (approximation + (x / approximation)) / 2
+        approximation = (approximation + (to_square_root / approximation)) / 2
     print("The square root is approximately: ", approximation)
 
 
 def sequence():
     number_of_terms = eval(input("How many terms would you like?"))
-    for i in range(1, number_of_terms + 1, 2):
-        print(i, i, end=" ")
-
+    for i in range(1, number_of_terms + 1):
+        print((i-1) + (i % 2), end=" ")
 
 
 def pi():
-    pass
+    terms = eval(input("how many terms in this series? "))
+    accumulator = 1
+    for i in range(terms):
+        numerator = i + (2.0 - (i % 2.0))
+        denominator = i + (1.0 + (i % 2.0))
+        accumulator = accumulator * (numerator/denominator)
+    your_pi = accumulator * 2
+    print(your_pi)
 
 
 if __name__ == '__main__':
