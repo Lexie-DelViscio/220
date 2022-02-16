adae45c5e6f7b660281ce4cbcd68e3ec0ef9f862
Lexie-DelViscio <delvisciolp@g.cofc.edu>
2022-02-16 17:05:17 -0500

    lab 5 commit

--- labs/lab5/lab5.py
+++ labs/lab5/lab5.py
@@ -1,5 +1,64 @@
-def triange():
-    pass
+"""
+Name: Lexie DelViscio
+File Name: lab5.py
+Problem Description: This program creates multiple different graphcis, and string/ list outputs. First there is a
+triangle with perimeter and area displayed, next a shape to be colored by a user, and a target for the grpahics
+related functions. For strings and lists there is an analysis of a string, analysis of a list, and another series
+evaluation.
+
+Certificate of Authenticity:
+I certify that this assignment is entirely my own work
+"""
+
+from graphics import *
+
+from math import sqrt
+
+
+def triangle():
+    win = GraphWin("Triangle", 700, 500)
+    win.setCoords(0, 0, 10, 10)
+    message = Text(Point(5, 0.5), "Click three times")
+    message.draw(win)
+
+    point1 = win.getMouse()
+    point1.draw(win)
+    point2 = win.getMouse()
+    point2.draw(win)
+    point3 = win.getMouse()
+    point3.draw(win)
+
+    p = Polygon(point1, point2, point3)
+    p.setFill("green")
+    p.setOutline("blue")
+    p.draw(win)
+
+    dx_side1 = abs(point1.getX() - point2.getX())
+    dy_side1 = abs(point1.getY() - point2.getY())
+    length_side1 = sqrt(dx_side1 ** 2 + dy_side1 ** 2)
+    dx_side2 = abs(point1.getX() - point3.getX())
+    dy_side2 = abs(point1.getY() - point3.getY())
+    length_side2 = sqrt(dx_side2 ** 2 + dy_side2 ** 2)
+    dx_side3 = abs(point2.getX() - point3.getX())
+    dy_side3 = abs(point2.getY() - point3.getY())
+    length_side3 = sqrt(dx_side3 ** 2 + dy_side3 ** 2)
+    perimeter = length_side1 + length_side2 + length_side3
+
+    perimeter_message = Text(Point(5, 1), round(perimeter, 3))
+    perimeter_message_2 = Text(Point(4.2, 1), "perimeter = ")
+    perimeter_message.draw(win)
+    perimeter_message_2.draw(win)
+
+    s = (length_side1 + length_side2 + length_side3) / 2
+    area = sqrt(s * (s - length_side1) * (s - length_side2) * (s - length_side3))
+    area_message = Text(Point(5, 1.5), round(area, 3))
+    area_message_2 = Text(Point(4.2, 1.5), "area = ")
+    area_message.draw(win)
+    area_message_2.draw(win)
+
+    message.setText("click anywhere to close")
+    win.getMouse()
+    win.close()
 
 
 def color_shape():
@@ -42,22 +101,120 @@ def color_shape():
     green_text.draw(win)
     blue_text.draw(win)
 
+    red_color_entry_point = Point(win_width / 2 - 10, win_height / 2 + 40)
+    red_color_entry = Entry(red_color_entry_point, 5)
+    red_color_entry.setText("")
+    red_color_entry.draw(win)
+
+    green_color_entry_point = Point(win_width / 2 - 10, win_height / 2 + 70)
+    green_color_entry = Entry(green_color_entry_point, 5)
+    green_color_entry.setText("")
+    green_color_entry.draw(win)
+
+    blue_color_entry_point = Point(win_width / 2 - 10, win_height / 2 + 100)
+    blue_color_entry = Entry(blue_color_entry_point, 5)
+    blue_color_entry.setText("")
+    blue_color_entry.draw(win)
+
+    for click in range(5):
+        win.getMouse()
+        red_string_value = red_color_entry.getText()
+        red_color_num = eval(red_string_value)
+        green_string_value = green_color_entry.getText()
+        green_color_num = eval(green_string_value)
+        blue_string_value = blue_color_entry.getText()
+        blue_color_num = eval(blue_string_value)
+        shape.setFill(color_rgb(red_color_num, green_color_num, blue_color_num))
+
     # Wait for another click to exit
     win.getMouse()
     win.close()
 
 
 def process_string():
-    pass
+    user_string = input("enter a string:")
+    first_character = user_string[0]
+    print(first_character)
+    last_character = user_string[-1]
+    print(last_character)
+    character_positions = user_string[1:5]
+    print(character_positions)
+    first_and_last = first_character + last_character
+    print(first_and_last)
+    first_three = user_string[0:3]
+    first_three_repeating = first_three * 10
+    print(first_three_repeating)
+    for user_value in user_string:
+        print(user_value, end="\n")
+    length_user_string = len(user_string)
+    print(length_user_string)
 
 
 def process_list():
-    pass
+    pt = Point(5, 10)
+    values = [5, "hi", 2.5, "there", pt, "7.2"]
+    x = values[1] + values[3]
+    print(x)
+    x = values[0] + values[2]
+    print(x)
+    x = values[1] * 5
+    print(x)
+    x = values[2:5:]
+    print(x)
+    x = values[2:4]
+    x = x + [values[0]]
+    print(x)
+    x = x[0::2]
+    x = x + [float(values[5])]
+    print(x)
+    x = x[0] + x[1] + x[2]
+    print(x)
+    x = len(values)
+    print(x)
 
 
 def another_series():
-    pass
+    n_another_series = eval(input("how many terms in the series:"))
+    another_series_list = []
+    for num in range(n_another_series):
+        series_value = ((num % 3) * 2 + 2)
+        another_series_list = another_series_list + [series_value]
+    print(another_series_list)
+    sum_another_series = sum(another_series_list)
+    print("sum=", sum_another_series)
 
 
 def target():
-    pass
+    win = GraphWin("Target", 1000, 1000)
+    win.setBackground("black")
+    center_point_white = Point(500,500)
+    radius_white = 500
+    white_circle = Circle(center_point_white, radius_white)
+    white_circle.setFill("white")
+    white_circle.draw(win)
+    center_point_black = Point(500, 500)
+    radius_black = 400
+    black_circle = Circle(center_point_black, radius_black)
+    black_circle.setFill("black")
+    black_circle.draw(win)
+    center_point_blue = Point(500, 500)
+    radius_blue = 300
+    blue_circle = Circle(center_point_blue, radius_blue)
+    blue_circle.setFill("blue")
+    blue_circle.draw(win)
+
+    center_point_red = Point(500, 500)
+    radius_red = 200
+    red_circle = Circle(center_point_red, radius_red)
+    red_circle.setFill("red")
+    red_circle.draw(win)
+    center_point_yellow = Point(500, 500)
+    radius_yellow = 100
+    yellow_circle = Circle(center_point_yellow, radius_yellow)
+    yellow_circle.setFill("yellow")
+    yellow_circle.draw(win)
+
+    click_to_close = Text(Point(500, 500), "Click to close")
+    click_to_close.draw(win)
+    win.getMouse()
+    win.close()
