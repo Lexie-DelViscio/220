9047cadaaec40a5803a48f46d8d23722ce7781b3
Lexie-DelViscio <delvisciolp@g.cofc.edu>
2022-03-03 20:57:33 -0500

    hw 7 and encryption py  commit

---
+++ assignments/hw7/encryption.py
@@ -0,0 +1,20 @@
+def encode(message, key):
+    encode_output = []
+    for letter in message:
+        encode_output.append(str(ord(letter) + key))
+    output = ""
+    for letters in encode_output:
+        output = output + chr(eval(letters))
+    return output
+
+
+def encode_better(message, key):
+    out_file = open("out_file.txt", 'w')
+    output_string = ""
+    for i in range(len(message)):
+        character_shift = ord(message[i]) - 65
+        shift_value = ord(key[i % len(key)]) - 65
+        shift_value_mod = (character_shift + shift_value) % 58
+        new_letter = chr(shift_value_mod + 65)
+        output_string = output_string + new_letter
+    return output_string
--- assignments/hw7/hw7.py
+++ assignments/hw7/hw7.py
@@ -1,42 +1,89 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Lexie DelViscio
+hw7.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: This homework utilizes the ability to open files for the purpose of reading
+and writing to them as well as performing this skill while creating functions. This
+program also combines encoding with functions and opening, reading, and writing files.
 
 Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+I certify that this assignment is my own work.
 """
+from encryption import encode, encode_better
 
 
 def number_words(in_file_name, out_file_name):
-    pass
+    in_file = open(in_file_name, 'r')
+    out_file = open(out_file_name, 'w')
+    file_string = in_file.read()
+    file_split = file_string.split()
+    numbered_string = ""
+    for i in range(len(file_split)):
+        numbered_string += str(i + 1) + " " + file_split[i] + "\n"
+    numbered_string = numbered_string.strip()
+    print(numbered_string, file=out_file)
 
 
 def hourly_wages(in_file_name, out_file_name):
-    pass
+    in_file = open(in_file_name, 'r')
+    out_file = open(out_file_name, 'w')
+    bonus = 1.65
+    file_split = in_file.readlines()
+
+    for i in file_split:
+        i = i.split(" ")
+        names = str(i[0] + " " + i[1])
+        hourly_wage = float(i[2])
+        number_hours = float(i[3])
+        hourly_bonus = hourly_wage + bonus
+        pay_for_week = hourly_bonus * number_hours
+        pay_for_week_formatted = "{:.2f}".format(pay_for_week)
+        employee_list = names + " " + pay_for_week_formatted
+        print(employee_list, file=out_file)
 
 
 def calc_check_sum(isbn):
-    pass
+    isbn = isbn.replace("-", "")
+    check_nums = []
+    for i in range(10, 0, -1):
+        check_nums += [i]
+    total_sum = 0
+    for i in range(len(isbn)):
+        partial_sum = int(isbn[i]) * check_nums[i]
+        total_sum += partial_sum
+    return total_sum
 
 
 def send_message(file_name, friend_name):
-    pass
-
-
-def encode():
-    pass
+    file = open(file_name, 'r')
+    out_file_name = friend_name + ".txt"
+    friend_file = open(out_file_name, 'w')
+    file_words = file.read()
+    file_words = file_words.strip()
+    print(file_words, file=friend_file)
 
 
 def send_safe_message(file_name, friend_name, key):
-    pass
+    file = open(file_name, 'r')
+    out_file_name = friend_name + ".txt"
+    friend_file = open(out_file_name, 'w')
+    orig_file = file.read()
+    new_file = encode(orig_file, key)
+    new_line_ord = ord("\n") + key
+    new_line_chr = chr(new_line_ord)
+    new_file = new_file.replace(new_line_chr, "\n")
+    print(new_file.rstrip(), file=friend_file)
 
 
 def send_uncrackable_message(file_name, friend_name, pad_file_name):
-    pass
+    file = open(file_name, 'r')
+    out_file_name = friend_name + ".txt"
+    friend_file = open(out_file_name, 'w')
+    pad_file_import = open(pad_file_name, 'r')
+    orig_file = file.read()
+    pad_file = pad_file_import.read()
+    new_file = encode_better(orig_file, pad_file)
+    print(new_file, file=friend_file)
 
 
 if __name__ == '__main__':
